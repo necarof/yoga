2c183232c0081e2cb43cb851da90769a82ccbaff
Nikita <43652463+necarof@users.noreply.github.com>
2020-10-01 17:22:17 +0600

    add calc

--- js/main.js
+++ js/main.js
@@ -198,4 +198,47 @@ window.addEventListener('DOMContentLoaded', function() {
     }
   });
 
+  // Calc
+
+  let persones = document.querySelectorAll('.counter-block-input')[0],
+      restDays = document.querySelectorAll('.counter-block-input')[1],
+      place = document.getElementById('select'),
+      totalValue = document.getElementById('total'),
+      personsSum = 0,
+      daysSum = 0,
+      total = 0;
+
+  totalValue.innerHTML = 0;
+
+  persones.addEventListener('change', function() {
+    personsSum =+ this.value;
+    total = (daysSum + personsSum) * 4000;
+
+    if (restDays.value == '' || persones.value == '') {
+      totalValue.innerHTML = 0;
+    } else {
+      totalValue.innerHTML = total;
+    };
+  });
+
+  restDays.addEventListener('change', function() {
+    daysSum =+ this.value;
+    total = (daysSum + personsSum) * 4000;
+
+    if (restDays.value == '' || persones.value == '') {
+      totalValue.innerHTML = 0;
+    } else {
+      totalValue.innerHTML = total;
+    };
+  });
+
+  place.addEventListener('change', function() {
+    if (restDays.value == '' || persones.value == '') {
+      totalValue.innerHTML = 0;
+    } else {
+      let a = total;
+      totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+    }
+  });
+
 });
