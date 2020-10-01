1dc0f28d8328d56e7c6e2873b3eb6e0c7e8f9c00
Nikita <43652463+necarof@users.noreply.github.com>
2020-10-01 15:07:02 +0600

    add slider

--- js/main.js
+++ js/main.js
@@ -127,7 +127,7 @@ window.addEventListener('DOMContentLoaded', function() {
     form.appendChild(statusMessage);
 
     let request = new XMLHttpRequest();
-    request.open('POST', 'http://localhost:8080');
+    request.open('POST', 'server.php');
     request.setRequestHeader('Content-Type', 'application/x-www-form-unlencoded');
 
     let formData = new FormData(form);
@@ -148,4 +148,54 @@ window.addEventListener('DOMContentLoaded', function() {
     }
   });
 
-})
+  // Slider
+
+  let slideIndex = 1,
+      slides = document.querySelectorAll('.slider-item'),
+      prev = document.querySelector('.prev'),
+      next = document.querySelector('.next'),
+      dotsWrap = document.querySelector('.slider-dots'),
+      dots = document.querySelectorAll('.dot');
+  
+  showSlides(slideIndex);
+  function showSlides(n) {
+
+    if (n > slides.length) {
+      slideIndex = 1;
+    };
+
+    if (n < 1) {
+      slideIndex = slides.length;
+    };
+
+    slides.forEach((item) => item.style.display = 'none');
+    dots.forEach((item) => item.classList.remove('dot-active'));
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].classList.add('dot-active');
+  };
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  };
+
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  };
+
+  prev.addEventListener('click', function() {
+    plusSlides(-1);
+  });
+
+  next.addEventListener('click', function() {
+    plusSlides(1);
+  });
+
+  dotsWrap.addEventListener('click', function(event) {
+    for (let i = 0; i < dots.length + 1; i++) {
+      if (event.target.classList.contains('dot') && event.target == dots[i - 1]) {
+        currentSlide(i);
+      }
+    }
+  });
+
+});
